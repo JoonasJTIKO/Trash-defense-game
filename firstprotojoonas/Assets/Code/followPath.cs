e760167e6756a12ca40bba189536cc909192c127
Joonas <joonas.jarvinen@tuni.fi>
2022-02-20 15:06:39 +0200

    added firstprotojoonas for real this time

---
+++ firstprotojoonas/Assets/Code/followPath.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace proto1
+{
+    public class followPath : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform[] waypoints;
+
+        [SerializeField]
+        private float speed = 1;
+
+        private int currentWaypoint = 0;
+        // Start is called before the first frame update
+        
+        void Start()
+        {
+            transform.position = waypoints[currentWaypoint].transform.position;
+            foreach (Transform child in waypoints)
+            {
+                child.SetParent(null, true);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            if(currentWaypoint <= waypoints.Length - 1)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypoint].transform.position, speed * Time.deltaTime);
+                if (transform.position == waypoints[currentWaypoint].transform.position)
+                {
+                    currentWaypoint += 1;
+                }
+            }
+        }
+    }
+}
+
