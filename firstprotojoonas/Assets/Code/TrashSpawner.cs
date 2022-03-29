7c5bed72454023579bac3df0fa2be3d245f16722
Watertuna <santeri.pelkonen@hotmail.com>
2022-03-29 10:37:13 +0300

    Trash spawn "animation"

--- firstprotojoonas/Assets/Code/TrashSpawner.cs
+++ firstprotojoonas/Assets/Code/TrashSpawner.cs
@@ -21,28 +21,35 @@ namespace towerdefense
         [SerializeField]
         int paperAmount;
         public GameObject paperPrefab;
+
+        [SerializeField]
+        private float spawnInterval;
         void Start()
         {
-            Invoke("spawnTrash", 0.4f);            
+            StartCoroutine(spawnTrash());
         }
 
-        void spawnTrash()
+        public IEnumerator spawnTrash()
         {
             for (int i = 0; i < cardboardAmount; i++)
             {
                 Instantiate(cardBoardPrefab, new Vector3(Random.Range(-4.5f, 4.5f), Random.Range(-4.5f, 4.5f), 1), Quaternion.identity);
+                yield return new WaitForSeconds(spawnInterval);
             }
             for (int i = 0; i < bioAmount; i++)
             {
                 Instantiate(bioPrefab, new Vector3(Random.Range(-4.5f, 4.5f), Random.Range(-4.5f, 4.5f), 1), Quaternion.identity);
+                yield return new WaitForSeconds(spawnInterval);
             }
             for (int i = 0; i < plasticAmount; i++)
             {
                 Instantiate(plasticPrefab, new Vector3(Random.Range(-4.5f, 4.5f), Random.Range(-4.5f, 4.5f), 1), Quaternion.identity);
+                yield return new WaitForSeconds(spawnInterval);
             }
             for (int i = 0; i < paperAmount; i++)
             {
                 Instantiate(paperPrefab, new Vector3(Random.Range(-4.5f, 4.5f), Random.Range(-4.5f, 4.5f), 1), Quaternion.identity);
+                yield return new WaitForSeconds(spawnInterval);
             }
         }
     }
